455c969967e4f4171d75cb92dbd00d60887a171f
SayanPark <s.poormanesh2002@gmail.com>
2025-01-10 00:39:59 +0330

    modified:   index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import {Universe} from "wasm-game-of-life";
-=======
-import * as wasm from "webassembly";
->>>>>>> ec9e63f5bf16d183fdb0d9536e0b9ab582110c96
 
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
